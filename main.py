b08733633de7b5708edff43246e99025a2ab034f
maassikll <94725508+maassikll@users.noreply.github.com>
2021-12-08 21:08:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#you must dowload PyWhatKit_DB
+#enjot
 import pyautogui,time,pywhatkit
-pywhatkit.sendwhatmsg("number_phone","salut je suis le bot spammeur ....",21,1,10)
+#enter the number when you want send the spam
+#the number 21 and 1 correspond is the time when the message will sent
+pywhatkit.sendwhatmsg("number_phone","hello im the bot spammer ....",21,1,10)
 time.sleep(10)
 for i in range(100):
-     pyautogui.write("je taime <<<<<")
+     pyautogui.write("i love you")
+     #press enter in the keyboard
      pyautogui.press("enter")
-
