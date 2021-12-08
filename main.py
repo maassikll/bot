0ec600de88453fbf2350676afe4772ae7fa735a0
maassikll <94725508+maassikll@users.noreply.github.com>
2021-12-08 21:05:40 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pyautogui,time,pywhatkit
+pywhatkit.sendwhatmsg("number_phone","salut je suis le bot spammeur ....",21,1,10)
+time.sleep(10)
+for i in range(100):
+     pyautogui.write("je taime <<<<<")
+     pyautogui.press("enter")
+
